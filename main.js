06232419f1c989b131417498da661f0114f591f2
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-16 01:33:39 +0200

    second

---
+++ main.js
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { sendError, errorStore } from "./errorHandler.js";
+
+//const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
+sendError("piss", "dette er crayzy");
