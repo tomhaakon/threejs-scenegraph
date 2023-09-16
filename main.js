d69190e981003ee4c623b2305506edeb28b50443
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-16 09:40:09 +0200

    moon

--- main.js
+++ main.js
@@ -49,14 +49,31 @@ function main() {
   solarSystem.add(sunMesh)
   objects.push(sunMesh)
 
+  const earthOrbit = new THREE.Object3D()
+  earthOrbit.position.x = 10
+  solarSystem.add(earthOrbit)
+  objects.push(earthOrbit)
+
   const earthMaterial = new THREE.MeshPhongMaterial({
     color: 0x2233ff,
     emissive: 0x112244,
   })
   const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial)
-  earthMesh.position.x = 10
-  solarSystem.add(earthMesh)
+  earthOrbit.add(earthMesh)
   objects.push(earthMesh)
+
+  const moonOrbit = new THREE.Object3D()
+  moonOrbit.position.x = 2
+  earthOrbit.add(moonOrbit)
+
+  const moonMaterial = new THREE.MeshPhongMaterial({
+    color: 0x888888,
+    emissive: 0x222222,
+  })
+  const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial)
+  moonMesh.scale.set(0.5, 0.5, 0.5)
+  moonOrbit.add(moonMesh)
+  objects.push(moonMesh)
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement
